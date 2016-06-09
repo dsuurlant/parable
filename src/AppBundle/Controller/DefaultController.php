589c908c6a53152f996eee1f45001b2c1978a482
Danielle Suurlant <danielle.suurlant@gmail.com>
2016-06-09 18:55:26 +0200

    Readding default controller and auto repo stub

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function defaultAction()
+    {
+        return $this->redirectToRoute("dashboard");
+    }
+}
