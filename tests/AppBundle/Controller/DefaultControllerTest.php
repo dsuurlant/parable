b616a84d572724522ee27920e3261712c61785dc
Danielle Suurlant <danielle.suurlant@gmail.com>
2016-12-18 22:15:35 +0100

    Some super simple tests, fixed #1

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testDefault()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        // should be a redirect.
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
 }
---
+++ tests/AppBundle/Controller/SecurityControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testLogin()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
